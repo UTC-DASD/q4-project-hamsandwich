124d6816d6b886b80472426bac3ba07072e2e373
duoplexgames <wyatt.acosta26@tech4maine.org>
2026-04-27 09:25:23 -0400

    Input Manager

--- Q4Project/Assets/Scripts/Player.cs
+++ Q4Project/Assets/Scripts/Player.cs
@@ -4,16 +4,16 @@ using System;
 public class Player : MonoBehaviour
 {
     // Physics Variables
-    double xspeed;
-    double yspeed;
-    double stopspeed;
-    double gravity;
-    double jumpforce;
+    public double xspeed;
+    public double yspeed;
+    public double stopspeed;
+    public double gravity;
+    public double jumpforce;
+
     // Update is called once per frame
     void Update()
     {
-        
-        if(InputMan.buttonA);
+        if(InputMan.buttonA)
         {
             xspeed = xspeed - 0.3;
             xspeed = Math.Clamp(xspeed, -3, 3);
