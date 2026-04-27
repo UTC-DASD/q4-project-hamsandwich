4cc8cabfd273e884fe05ea89d14221f2d807a93c
duoplexgames <wyatt.acosta26@tech4maine.org>
2026-04-27 10:14:19 -0400

    Updated Player.cs

--- Q4Project/Assets/Scripts/Player.cs
+++ Q4Project/Assets/Scripts/Player.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
-using System;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Physics Variables
-    public double xspeed;
-    public double yspeed;
-    public double stopspeed;
-    public double gravity;
-    public double jumpforce;
+    public float xspeed; // Horizontal Movement
+    public float yspeed; // Vertical Movement
+    public float stopspeed = 25f; // How Fast 2 Stop
+    public float jumpforce = 5f; // How High Two Jump
+    public bool gravity = true; // Which way is Gravity facing?
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        if(InputMan.buttonA)
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+    void FixedUpdate()
+    {
+        if(InputMan.buttonA) // If pressing A, move left
         {
-            xspeed = xspeed - 0.3;
-            xspeed = Math.Clamp(xspeed, -3, 3);
+            xspeed = xspeed - 10f * Time.fixedDeltaTime;
         }
+
+        if(InputMan.buttonD) // The sequel to moving left. Moving right!
+        {
+            xspeed = xspeed + 10f * Time.fixedDeltaTime;
+        }
+
+        if(!InputMan.buttonA && !InputMan.buttonD)
+        {
+            xspeed = Mathf.MoveTowards(xspeed, 0, stopspeed * Time.fixedDeltaTime);
+        }
+
+        xspeed = Mathf.Clamp(xspeed, -8.5f, 8.5f);
+        rb.linearVelocity = new Vector2(xspeed, rb.linearVelocity.y);
     }
 }
